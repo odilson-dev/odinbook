e8a00af5a33d4d05b40dd1bf792a22cff9b56512
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-24 16:30:33 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence:true, uniqueness: true
+  validates :birthdate, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\z/, message: "Invalid email format" }
+  validates :password, presence: true, length: { in: 6..20 }
 end
