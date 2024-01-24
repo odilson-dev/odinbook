63232498e28dc60912ede089796be8272436b624
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-24 11:54:18 -0500

    Create index actions in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
   
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
