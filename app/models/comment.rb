2ac1e07d18386ace10c27837991380a4908f49b9
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-30 09:25:14 -0500

    Add delete and edit features for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  
+  # This line is about making comments commentable themselves
   has_many :comments, as: :commentable, dependent: :destroy
 end
