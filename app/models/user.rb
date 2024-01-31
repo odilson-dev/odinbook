a9f1cfd2790fa65a57737697256b887310f9a015
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-31 15:58:24 -0500

    Attach images to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :image
   def unfollow(user)
     followerable_relationships.where(followable_id: user.id).destroy_all
   end
