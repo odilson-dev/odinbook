7b9c5e687f8e344bb39f4f2fe5f91e6e23afec06
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-05-29 12:40:33 -0400

    Add GitHub authentication process

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [:google_oauth2, :github]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
