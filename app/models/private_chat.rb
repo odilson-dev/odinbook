d8e2551e56b75676eb7df611d7bb9b3a5db15262
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-06-04 11:36:15 -0400

    Add model associations

--- app/models/private_chat.rb
+++ app/models/private_chat.rb
@@ -1,4 +1,5 @@
 class PrivateChat < ApplicationRecord
-  belongs_to :profile1
-  belongs_to :profile2
+  belongs_to :profile1, class_name: "Profile"
+  belongs_to :profile2, class_name: "Profile"
+  has_many :messages, dependent: :destroy
 end
