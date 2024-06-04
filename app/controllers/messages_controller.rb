fe0fc2fc8f31bb42fd1d7726845061d5a3f0cd20
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-06-04 12:03:07 -0400

    Create message controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,28 @@
+class MessagesController < ApplicationController
+  before_action :find_private_chat
+  
+  def create
+    @message = @private_chat.messages.new(message_params)
+    @message.profile_id = current_profile.id
+
+    if @message.save
+      PrivateChatChannel.broadcast_to(
+        @message.private_chat,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+    end
+
+    head :ok
+
+  end
+
+  private
+
+  def find_private_chat
+    @private_chat = PrivateChat.find(params[:private_chat_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
