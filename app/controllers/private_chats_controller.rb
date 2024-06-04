430bc7920a346cbc7282a0969e9ed1a020b3c428
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-06-04 11:54:46 -0400

    Generate private_chats controller

---
+++ app/controllers/private_chats_controller.rb
@@ -0,0 +1,26 @@
+class PrivateChatsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_private_chat, only: [:show]
+  def index
+    if current_profile
+      @profile = current_profile
+      @private_chats = PrivateChat.where("profile1_id = ? OR profile2_id = ?", @profile.id, @profile.id).order("created_at DESC")
+    else
+      redirect_to login_path, notice: 'Please log in first.'
+    end
+  end
+
+  def show
+    @profile = Profile.find(params[:profile_id])
+    @private_chat = PrivateChat.find(params[:id])
+    @message = Message.new
+  end
+
+  def find_private_chat
+    @private_chat = PrivateChat.find(params[:id])
+  end
+
+  def private_chat_params
+    params.require(:private_chat).permit(:profile1_id, :profile2_id)
+  end
+end
