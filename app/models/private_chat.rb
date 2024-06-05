71a743572b317b486f1cee5c89d67f9830c6d0c4
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-06-04 20:50:23 -0400

    Add class method on PrivateChat model

--- app/models/private_chat.rb
+++ app/models/private_chat.rb
@@ -2,4 +2,12 @@ class PrivateChat < ApplicationRecord
   belongs_to :profile1, class_name: "Profile"
   belongs_to :profile2, class_name: "Profile"
   has_many :messages, dependent: :destroy
+
+  def self.get_private_chat(profile1_id, profile2_id)
+    where(
+      "(profile1_id = :profile1_id AND profile2_id = :profile2_id) OR
+      (profile1_id = :profile2_id AND profile2_id = :profile1_id)",
+      profile1_id: profile1_id, profile2_id: profile2_id,
+    ).first
+  end
 end
