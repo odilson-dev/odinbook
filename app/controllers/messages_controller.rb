9714d8e16893c224985316807341aa08f181de9a
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-06-06 11:50:16 -0400

    Fix bug when creating and saving message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   
   def create
     @message = @private_chat.messages.new(message_params)
-    @message.profile_id = current_profile.id
+    @message.profile_id = current_user.profile.id
 
     if @message.save
       PrivateChatChannel.broadcast_to(
